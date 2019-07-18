71b2c842a812be27dadeff990b9e8a9f45c3bee8
Joao Leitao <joao.leitao@farfetch.com>
2019-07-18 18:51:52 +0100

    feat: Initial JsonFilter version

---
+++ JK.JsonFilter.App/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace JK.JsonFilter.App
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using JK.JsonFilter.Lib;
+    using McMaster.Extensions.CommandLineUtils;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Program
+    {
+        private readonly IConsole console;
+        private readonly CancellationTokenSource cancellationTokenSource;
+
+        public Program(
+            IConsole console,
+            CancellationTokenSource cancellationTokenSource)
+        {
+            this.console = console;
+            this.cancellationTokenSource = cancellationTokenSource;
+
+            this.console.CancelKeyPress += this.OnCancelKeyPress;
+        }
+
+        private enum ErrorCode
+        {
+            Success = 0,
+            UnknownError = -1,
+            OperationCanceled = -2,
+        }
+
+        [Option("-s|--selector", Description = "The JSON path field selector. Elements matching the selector will be included in the output.")]
+        [Required]
+        public string[] JsonFieldSelectors { get; }
+
+        public static int Main(string[] args)
+        {
+            var serviceProvider = BuildServiceProvider();
+
+            var app = new CommandLineApplication<Program>(serviceProvider.GetRequiredService<IConsole>());
+
+            app.Conventions
+                .UseDefaultConventions()
+                .UseConstructorInjection(serviceProvider);
+
+            app.OnExecute(app.Model.OnExecuteAsync);
+
+            return app.Execute(args);
+        }
+
+        private static IServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton(PhysicalConsole.Singleton);
+            services.AddSingleton<CancellationTokenSource>();
+
+            return services.BuildServiceProvider();
+        }
+
+        private async Task<int> OnExecuteAsync()
+        {
+            try
+            {
+                await JsonFilter.FilterJsonAsync(this.console.In, this.console.Out, this.JsonFieldSelectors).ConfigureAwait(false);
+
+                return (int)ErrorCode.Success;
+            }
+            catch (OperationCanceledException)
+            {
+                return (int)ErrorCode.OperationCanceled;
+            }
+            catch (Exception ex)
+            {
+                await this.WriteExceptionAsync(ex).ConfigureAwait(false);
+
+                return (int)ErrorCode.UnknownError;
+            }
+        }
+
+        private void OnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            this.console.Error.WriteLine("Operation cancellation requested.");
+            this.cancellationTokenSource.Cancel();
+        }
+
+        private async Task WriteExceptionAsync(Exception ex)
+        {
+            await this.console.Error.WriteLineAsync($"{ex.GetType().Name}: {ex.Message}").ConfigureAwait(false);
+
+            foreach (var dataKey in ex.Data.Keys)
+            {
+                await this.console.Error.WriteLineAsync($"{dataKey}: {ex.Data[dataKey]}").ConfigureAwait(false);
+            }
+
+            await this.console.Error.WriteLineAsync($"StackTrace:{Environment.NewLine}{ex.StackTrace}").ConfigureAwait(false);
+        }
+    }
+}
---
+++ JK.JsonFilter.Lib/JsonFilter.cs
@@ -0,0 +1,77 @@
+﻿namespace JK.JsonFilter.Lib
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json.Linq;
+
+    public static class JsonFilter
+    {
+        public static Task FilterJsonAsync(TextReader textReader, TextWriter textWriter, string[] fieldSelectors)
+        {
+            if (textReader == null)
+            {
+                throw new ArgumentNullException(nameof(textReader));
+            }
+
+            if (textWriter == null)
+            {
+                throw new ArgumentNullException(nameof(textWriter));
+            }
+
+            if (fieldSelectors == null)
+            {
+                throw new ArgumentNullException(nameof(fieldSelectors));
+            }
+
+            return FilterJsonInternalAsync(textReader, textWriter, fieldSelectors);
+        }
+
+        private static async Task FilterJsonInternalAsync(TextReader textReader, TextWriter textWriter, string[] fieldSelectors)
+        {
+            var json = await textReader.ReadToEndAsync().ConfigureAwait(false);
+
+            json = FilterJson(json, fieldSelectors);
+
+            await textWriter.WriteAsync(json).ConfigureAwait(false);
+        }
+
+        private static string FilterJson(string json, string[] fieldSelectors)
+        {
+            var jsonObject = JToken.Parse(json);
+
+            var whiteListedPaths = fieldSelectors
+                .SelectMany(path => jsonObject.SelectTokens(path))
+                .Where(token => !string.IsNullOrEmpty(token.Path))
+                .Select(token => token.Path)
+                .ToArray();
+
+            FilterObject(jsonObject, ref whiteListedPaths);
+
+            return jsonObject.ToString();
+        }
+
+        private static void FilterObject(JToken jsonObject, ref string[] whiteListedPaths)
+        {
+            foreach (var token in jsonObject.ToArray())
+            {
+                var currentPath = token.Path;
+                if (!whiteListedPaths.Any(path => IsPathIncluded(path, currentPath)))
+                {
+                    token.Remove();
+                    continue;
+                }
+
+                FilterObject(token, ref whiteListedPaths);
+            }
+        }
+
+        private static bool IsPathIncluded(string fullPath, string candidatePath)
+        {
+            return fullPath.Equals(candidatePath)
+                || fullPath.StartsWith($"{candidatePath}.")
+                || fullPath.StartsWith($"{candidatePath}[");
+        }
+    }
+}
